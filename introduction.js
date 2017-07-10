4d86b271d4521dc00b4f4c3359c283642c7eb5f0
mathieu regourd <mathieu.rgrd@gmail.com>
2017-07-10 16:43:22 +0200

    My first commit on JS! Yay !

---
+++ introduction.js
@@ -0,0 +1,20 @@
+  
+ 
+ likePotatoes = new confirm();
+ likePotatoes = confirm("Hey mon ami tu aime les patates ?");
+
+ alert(likePotatoes);
+  
+  if (likePotatoes) {
+    
+    Potatoe = "#";
+    
+    i = 1;
+    
+    
+    for(i = 1; i < 8; i ++){
+      console.log(i * Potatoe)
+    }
+  } else {
+    alert("Je vous sens comme tiraillé ! ")
+  }
